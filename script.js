96bdcce04ca828b9e846dcfc17e57ea8f66795bf
Metikotakusuma <110721413+Metikotakusuma@users.noreply.github.com>
2022-08-23 02:39:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var header =document.querySelector("a")
+header.style.color="blue"
+function getRandomColor()
+{
+    var letters ="0123456789ABCDEF";
+    var color="#";
+    for(var i=0;i<6;i++)
+    {
+        color +=letters[Math.floor(Math.random()*16)];
+    }
+    return color
+}
+function changeHeaderColor()
+{
+    colorInput =getRandomColor()
+    header.style.color = colorInput;
+}
+setInterval("changeHeaderColor()",500);
+function SHOP(){
+    alert("add some product to cart")
+}
+function ADD(){
+    alert("added to cart")
+}
